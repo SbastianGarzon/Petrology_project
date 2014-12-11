ec9877c51add45a14456c04e37de693d4ecb2e83
Sebastian Garzon <js.garzon398@uniandes.edu.co>
2014-12-11 02:12:35 -0500

    Problems with the graphics in .py

--- Major_elements/TAS_diagram.py
+++ Major_elements/TAS_diagram.py
@@ -1,19 +1,29 @@
-import matplotlib.pyplot as plt
+import numpy
+import matplotlib
+from matplotlib import pylab, mlab, pyplot
+np = numpy
+plt = pyplot
+
+from IPython.display import display
+from IPython.core.pylabtools import figsize, getfigs
+
+from pylab import *
+from numpy import *
 
 def TAS():
     fig = plt.figure(figsize=(16,10.28))
     ax = fig.add_subplot(111)
     p1=ax.text(37, 10, '$Foidite$', fontsize=20)
-    p2=ax.text(41.5, 1, '$ Picro-$ \n$basalt $', fontsize=20)
+    p2=ax.text(41.5, 1, '$ Picro-$\n$basalt $', fontsize=20)
     p3=ax.text(47, 2.5, '$Basalt$', fontsize=20)
     p4=ax.text(55, 14, '$Phonolite$', fontsize=20)
-    p5=ax.text(51, 11.5, '$Tephri-$\n $phonolite$', fontsize=20)
-    p6=ax.text(52.3, 3, '$Basaltic$\n $Andesite$', fontsize=20)
+    p5=ax.text(51, 11.5, '$Tephri-$\n$phonolite$', fontsize=20)
+    p6=ax.text(52.3, 3, '$Basaltic$\n$Andesite$', fontsize=20)
     p7=ax.text(58, 3, '$Andesite$', fontsize=20)
     p8=ax.text(66, 5, '$Dacite$', fontsize=20)
-    p9=ax.text(42, 6, '$Tephrite$\n $Basanite$', fontsize=20)
-    p10=ax.text(47, 5.1, '$Tachy-$\n $basalt$', fontsize=20)
-    p11=ax.text(47, 9, '$Phono-$\n $tephrite$', fontsize=20)
+    p9=ax.text(42, 6, '$Tephrite$\n$Basanite$', fontsize=20)
+    p10=ax.text(47, 5.1, '$Tachy-$\n$basalt$', fontsize=20)
+    p11=ax.text(47, 9, '$Phono-$\n$tephrite$', fontsize=20)
     p12=ax.text(50.7, 6.1, '$Basaltic$\n$trachy-$\n$andesite$', fontsize=20)
     p13=ax.text(56, 8, '$Trachy-$\n$andesite$', fontsize=20)
     p14=ax.text(62, 12, '$Trachyte$', fontsize=20)
@@ -46,3 +56,10 @@ def TAS():
     plt.xlim(35, 80)
     plt.ylim(0,16)
     plt.title('TAS diagram. Lemaitre',fontsize=25)
+    #ejemplo
+    x=[50,60,70,45]
+    y=[8,9,10,11]
+    p1=plt.scatter(x,y,s=500,alpha=0.5,color='g',marker='D')
+    plt.legend([p1, p2,p3], ["Caribbean-Colombia Plateau "])
+    plt.savefig('TAS_Wii.jpg')
+TAS()
